f0cd686f182b94855c85181418fd3c551f06e734
Thomas Rusiecki <rusieckithomas@gmail.com>
2026-04-14 00:05:13 -0300

    separate resolve url and did json fetch

--- pyvckit/did.py
+++ pyvckit/did.py
@@ -114,33 +114,45 @@ def gen_did_document(did, keys):
     url = "https://" + "/".join(did.split(":")[2:]) + "/did.json"
     return url, document_fixed_serialized
 
-
-def resolve_did(did, verify=True):
-    if did[:8] != "did:web:":
-        return
+def resolve_did_url(did):
+    if not did.startswith("did:web:"):
+        return None
 
     sdid = did[8:].split(":")
     domain = sdid[0]
     paths = sdid[1:]
 
     if not paths:
-        url = f"https://{domain}/.well-known/did.json"
+        return f"https://{domain}/.well-known/did.json"
     else:
         url_path = "/".join(paths)
-        url = f"https://{domain}/{url_path}/did.json"
+        return f"https://{domain}/{url_path}/did.json"
+
+
+def resolve_did(did, verify=True):
+    url = resolve_did_url(did)
+
+    if not url:
+        return None
 
     try:
         response = requests.get(url, verify=verify)
+        response.raise_for_status()
     except Exception:
         url = url.replace("https://", "http://")
         try:
             response = requests.get(url, verify=verify)
+            response.raise_for_status()
         except Exception:
             return None
 
     if 200 <= response.status_code < 300:
-        return response.json()
+        try:
+            return response.json()
+        except ValueError:
+            return None
 
+    return None
 
 def main():
     parser=argparse.ArgumentParser(description='Generates a new did or key pair')
